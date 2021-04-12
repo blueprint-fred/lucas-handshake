c4ee7be62b0b17dc0e20f8d46f4dc778c624f684
Fredrick Garingo <fredrickjohng6@gmail.com>
2021-04-12 17:45:12 +0800

    init design and content

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,120 +1,47 @@
-import * as React from 'react';
-import {ProgressSteps, NumberedStep} from 'baseui/progress-steps';
-import {Button, SHAPE, KIND, SIZE} from 'baseui/button';
-import {useStyletron} from 'baseui';
-import { MDBCol, MDBContainer, MDBRow } from 'mdbreact';
-import { Input } from "baseui/input";
-import { Textarea } from "baseui/textarea";
+import { MDBCol, MDBContainer, MDBRow } from 'mdbreact'
+import React from 'react'
+import { StyledLink } from "baseui/link";
 
-function SpacedButton(props) {
-  return (
-    <Button
-      {...props}
-      shape={SHAPE.pill}
-      kind={KIND.secondary}
-      size={SIZE.compact}
-      overrides={{
-        BaseButton: {
-          style: ({$theme}) => ({
-            marginLeft: $theme.sizing.scale200,
-            marginRight: $theme.sizing.scale200,
-            marginTop: $theme.sizing.scale800,
-          }),
-        },
-      }}
-    />
-  );
-}
-function AboutPage() {
-  const [current, setCurrent] = React.useState(0);
-  const [value, setValue] = React.useState("")
-  const [css, theme] = useStyletron();
-  return (
-      <MDBContainer className="py-5 my-5">
-        <form className="py-4" action="">
-        <ProgressSteps current={current}>
-        <NumberedStep title="Personal Information">
-            <div className={css({...theme.typography.ParagraphSmall})}>
-            Here is some step content
-            </div>
+const AboutPage = () => {
+    return (
+        <MDBContainer className="py-5 my-5">
             <MDBRow>
-                <MDBCol md="8" lg="8">
-                    <MDBRow className="py-2">
-                    <MDBCol md="6" lg="6">
-                        <Input
-                            value={value}
-                            onChange={e => setValue(e.target.value)}
-                            placeholder="First Name"
-                            clearOnEscape
-                        />
-                    </MDBCol>
-                    <MDBCol md="6" lg="6">
-                        <Input
-                            value={value}
-                            onChange={e => setValue(e.target.value)}
-                            placeholder="Last Name"
-                            clearOnEscape
-                        />
-                    </MDBCol>
-                </MDBRow>
-                <MDBRow className="py-2">
-                    <MDBCol md="6" lg="6">
-                        <Input
-                            value={value}
-                            onChange={e => setValue(e.target.value)}
-                            placeholder="Email"
-                            clearOnEscape
-                        />
-                    </MDBCol>
-                    <MDBCol md="6" lg="6">
-                        <Input
-                            value={value}
-                            onChange={e => setValue(e.target.value)}
-                            placeholder="Company"
-                            clearOnEscape
-                        />
-                    </MDBCol>
-                </MDBRow>
-                <MDBRow className="py-2">
-                    <MDBCol>
-                    <Textarea
-                        value={value}
-                        onChange={e => setValue(e.target.value)}
-                        placeholder="Reason for using handshake"
-                        clearOnEscape
-                    />
-                    </MDBCol>
-                </MDBRow>
+                <MDBCol>
+                    <h1 className="h1-responsive font-weight-bold">
+                        What's the purpose of this application?
+                    </h1>
+                    <div className="content my-4">
+                        <p className="lead">
+                            The main purpose of this application is to help people create content for Dojo, especially get recognition to audiences from different kinds of social media platforms.
+                            Educate people from here and the application does it's job to spread the good word out! We are moving forward in to the digital world. Everything and any content will be published
+                            and used for the greater good.
+                        </p>
+                    </div>
                 </MDBCol>
             </MDBRow>
-            <SpacedButton disabled>Previous</SpacedButton>
-            <SpacedButton onClick={() => setCurrent(1)}>
-            Next
-            </SpacedButton>
-        </NumberedStep>
-        <NumberedStep title="Choosing a template">
-            <div className={css({...theme.typography.ParagraphSmall})}>
-            Here is some more content
-            </div>
-            <SpacedButton onClick={() => setCurrent(0)}>
-            Previous
-            </SpacedButton>
-            <SpacedButton onClick={() => setCurrent(2)}>
-            Next
-            </SpacedButton>
-        </NumberedStep>
-        <NumberedStep title="Content Management">
-            <div className={css({...theme.typography.ParagraphSmall})}>
-            Here too!
-            </div>
-            <SpacedButton onClick={() => setCurrent(1)}>
-            Previous
-            </SpacedButton>
-            <SpacedButton disabled>Next</SpacedButton>
-        </NumberedStep>
-        </ProgressSteps>
-        </form>
-      </MDBContainer>
-  );
+            <MDBRow className="py-3">
+                <MDBCol>
+                    <h3 className="h3-responsive font-weight-bold">
+                        Check out our other web applications
+                    </h3>
+                    <div className="content my-3">
+                        <StyledLink href="https://baseweb.design">
+                            Dojo
+                        </StyledLink><br/>
+                        <StyledLink href="https://baseweb.design">
+                            Devasuki
+                        </StyledLink><br/>
+                        <StyledLink href="https://baseweb.design">
+                            Real Raw Talent
+                        </StyledLink><br/>
+                        <StyledLink href="https://baseweb.design">
+                            AIT
+                        </StyledLink>
+                    </div>
+                </MDBCol>
+            </MDBRow>
+        </MDBContainer>
+    )
 }
-export default AboutPage;
+
+export default AboutPage
