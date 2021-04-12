0ddfdac55e2af7bcc25e5ba716ff93c92336d1a3
Fredrick Garingo <fredrickjohng6@gmail.com>
2021-04-12 17:45:31 +0800

    added contact route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Navbar from './components/Navbar';
 import Index from './pages';
 import AboutPage from './pages/about';
+import ContactPage from './pages/contact';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Switch>
         <Route exact path="/" component={Index}/>
         <Route path="/about" component={AboutPage}/>
+        <Route path="/contact" component={ContactPage}/>
       </Switch>
     </Router>
   )
